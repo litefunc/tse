821cb620c62481ac5253824eedb54d3d54fcfdcf
litefunc <lite.func@gmail.com>
2019-09-15 16:46:46 +0800

    update tse

--- update_tse.py
+++ update_tse.py
@@ -14,13 +14,13 @@
 # 鉅額交易日成交資訊
 
 
-import crawler.finance.tse.鉅額交易.鉅額交易日成交資訊
 import crawler.finance.tse.盤後資訊.當日融券賣出與借券賣出成交量值
 import crawler.finance.tse.三大法人.三大法人買賣超日報
 import crawler.finance.tse.盤後資訊.個股日本益比殖利率及股價淨值比
 import crawler.finance.tse.盤後資訊.每日收盤行情1
+import crawler.finance.tse.鉅額交易.鉅額交易日成交資訊1
 
-import crawler.finance.tse.mongoToLite.鉅額交易日成交資訊1
+import crawler.finance.tse.mongoToLite.鉅額交易日成交資訊
 import crawler.finance.tse.mongoToLite.每日收盤行情
 
 from crawler.finance.sqliteToPostgres.update import *
